8d2a0a58f4cd42209f6da3cb9960b848582775fe
ysk-9 <yaswanthsaikiran9@gmail.com>
2025-05-31 20:48:38 +0530

    project final changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,10 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import {app,server} from './lib/socket.js';
 import { connectDB } from './lib/db.js';
-
+import path from "path";
 dotenv.config();
 const port = process.env.PORT;
-
+const __dirname= path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -20,6 +20,15 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../Frontend/WhisperBox/dist")))
+
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../Frontend/WhisperBox","dist","index.html"))
+  })
+}
 server.listen(port, () => {
   console.log('Server is running on port :' + port); 
   connectDB();
